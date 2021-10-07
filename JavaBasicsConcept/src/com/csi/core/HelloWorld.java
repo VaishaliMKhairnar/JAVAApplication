b24e6a2a1825bfcabc95050a20baaea55dc6b112
KiranKhot142 <kkhot142@gmail.com>
2021-10-07 15:21:44 +0530

    Changes Are Done

--- JavaBasicsConcept/src/com/csi/core/HelloWorld.java
+++ JavaBasicsConcept/src/com/csi/core/HelloWorld.java
@@ -3,5 +3,6 @@ package com.csi.core;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("WELCOME TO CSI PUNE");
+	System.out.println("Problem Solved !!!!");
 }
 }
